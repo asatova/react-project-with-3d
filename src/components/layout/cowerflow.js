0ae0452a3ad34c0ebc9826efe57684ee62262b3a
zarnigor <zarnigorasatova52@gmail.com>
2024-07-13 18:29:19 +0500

    feature

--- src/components/layout/cowerflow.js
+++ src/components/layout/cowerflow.js
@@ -1,19 +1,39 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-import './cowerflow.css'; 
-
+import './cowerflow.css'; // Ensure this file name is correct
 // import required modules
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules'; // Added Navigation import
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+import slide_image_1 from './images/1.jpg';
+
+const images = [
+    slide_image_1,
+    "https://swiperjs.com/demos/images/nature-2.jpg",
+    "https://swiperjs.com/demos/images/nature-3.jpg",
+    "https://swiperjs.com/demos/images/nature-4.jpg",
+    "https://swiperjs.com/demos/images/nature-5.jpg",
+    "https://swiperjs.com/demos/images/nature-6.jpg",
+    "https://swiperjs.com/demos/images/nature-7.jpg",
+    "https://swiperjs.com/demos/images/nature-8.jpg",
+    "https://swiperjs.com/demos/images/nature-9.jpg"
+];
 
 export default function App() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [photoIndex, setPhotoIndex] = useState(0);
+
+    const handleImageClick = (index) => {
+        setPhotoIndex(index);
+        setIsOpen(true);
+    };
+
     return (
         <>
             <Swiper
@@ -29,38 +49,30 @@ export default function App() {
                     slideShadows: true,
                 }}
                 pagination={true}
-                navigation={true} // Added navigation property
-                modules={[EffectCoverflow, Pagination, Navigation]} // Corrected modules array
+                navigation={true}
+                modules={[EffectCoverflow, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                </SwiperSlide>
+                {images.map((img, index) => (
+                    <SwiperSlide key={index}>
+                        <img src={img} alt={`Slide ${index}`} onClick={() => handleImageClick(index)} />
+                    </SwiperSlide>
+                ))}
             </Swiper>
+            {isOpen && (
+                <Lightbox
+                    mainSrc={images[photoIndex]}
+                    nextSrc={images[(photoIndex + 1) % images.length]}
+                    prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+                    onCloseRequest={() => setIsOpen(false)}
+                    onMovePrevRequest={() =>
+                        setPhotoIndex((photoIndex + images.length - 1) % images.length)
+                    }
+                    onMoveNextRequest={() =>
+                        setPhotoIndex((photoIndex + 1) % images.length)
+                    }
+                />
+            )}
         </>
     );
 }
